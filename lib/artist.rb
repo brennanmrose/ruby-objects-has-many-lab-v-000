a8b9dd36bbcbf3b2a08f63436816d43c7ebef6b5
Rose Brennan <brennan.m.rose@gmail.com>
2018-10-12 10:43:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,17 +24,18 @@ class Artist
     @songs
   end
 
-  # def self.song_count
-  #   count = 0 
-  #   @@all.each do |artist|
-  #     artist.songs.each do |post|
-  #       count += 1
-  #     end 
-  #   end 
-  #   count
-  # end
-  
   def self.song_count
-    Song.all.count
+    count = 0 
+    @@all.each do |artist|
+      artist.songs.each do |post|
+        count += 1
+      end 
+    end 
+    count
   end
 end
+
+
+  # def self.song_count
+  #   Song.all.count
+  # end
